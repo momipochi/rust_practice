f10876d26a7e87aecf9ad4c0eb20e107ec72fe15
momipochi <k25650727@gmail.com>
2023-12-17 10:19:38 +0100

    commit

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,23 +1,17 @@
+use minigrep::Config;
 use std::env;
 use std::process;
-
-use minigrep::Config;
-use minigrep::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    if let Err(e) = run(config) {
-        println!("Application error: {e}");
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
-
