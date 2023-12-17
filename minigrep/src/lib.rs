a7d99351e89fc7c88a69470a22de3ffbbb9823c3
momipochi <k25650727@gmail.com>
2023-12-17 02:32:47 +0100

    commit

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -20,12 +20,20 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
+    let _contents = fs::read_to_string(config.file_path)?;
 
     Ok(())
 }
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
